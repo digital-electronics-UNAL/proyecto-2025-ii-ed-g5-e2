ac20be24a7bcb27faed1396a165a7bfb4b22d43c
Abishek957951 <aarunachalam@unal.edu.co>
2025-11-25 00:05:06 -0500

    NADA

--- src/FSM_principal.v
+++ src/FSM_principal.v
@@ -1,1 +1,21 @@
+/*
+Aquí se va a encontrar la FSM principal que contiene toda la lógico de switcheo de los reles,
+y de la información que va a mostrar la lcd
+*/
 
+module FMS_principal();
+
+
+//Estados de la FSM
+
+
+
+
+
+
+
+
+
+
+
+endmodule
