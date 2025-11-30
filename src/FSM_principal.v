61e2cee3bfcb7fada62d88f1457de12985700120
Abishek957951 <aarunachalam@unal.edu.co>
2025-11-29 20:46:21 -0500

    rt

--- src/FSM_principal.v
+++ src/FSM_principal.v
@@ -3,11 +3,22 @@ Aquí se va a encontrar la FSM principal que contiene toda la lógico de switche
 y de la información que va a mostrar la lcd
 */
 
-module FMS_principal();
+module FMS_principal(
+    input clk,
+    input rst,
+    input [7:0] sen_1,  
+    input [7:0] sen_2, 
+    input [7:0] sen_3, 
+    output reg [3:0] state_out,
+    output reg [7:0] output_signal
+);
 
 
 //Estados de la FSM
-
+localparam STATE_IDLE    = 4'b0000;
+localparam STATE_HEAR_DETECTION = 4'b0001;
+localparam STATE_FAIL_DETECTED = 4'b0010;
+localparam STATE_DONE    = 4'b0011;
 
 
 
